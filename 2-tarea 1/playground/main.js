2bbacc30508a095c5e09934907eaa39aea1c69fb
jhoelda <jhoeldallanos@gmail.com>
2020-07-22 14:57:05 -0300

    tarea 4 ejercicio 6 funciones terminado

--- 2-tarea 1/playground/main.js
+++ 2-tarea 1/playground/main.js
@@ -1,34 +1,32 @@
 // Funciones de matriz de JavaScript
 // ejercicio 1
 //integrantes de Ctrol Z
-var className = ["Belen","Cata","Jhoelda","Luz","Maga","Yennir","Carla","Rodrigo","Branko"]
+var className = ["Belen","Catalina","Jhoelda","Luz","Magali","Yennyfer","Carla","Rodrigo","Branko","Andrea","Aylen","Azucena","Belen","Camila","Carolina","Florencia","Katy","Luana","Macarena","Malena","Maria","Micaela","Nadine","Nicole","Rocio","Reyna","Veronica","Victoria","Yael","Florencia","Janneth","Melissa","Florencia"]
 className.sort()//ordena className alfabeticamente
-console.log(className[0]);//primera posicion 
-console.log(className[className.length-1])//ultima posicion
-console.log("")
+console.log("Primer valor del array:",className[0]);//primera posicion 
+console.log("Ultimo valor del array:",className[className.length-1])//ultima posicion
 for(var i = 0; i < className.length; i++){
     console.log(className[i])
 }
 console.log("")//dejo un espacio para separar cada ejercicio en la consola
+
 // ejercicio 2
 var classAge = [20,21,22,23,20,19,24,18,22,20,23,21,23,26,32]
-i =0;
-//saco por consola todos las edades
+console.log("Todas las edades:")//saco por consola todos las edades
+var i =0;
 while(i<classAge.length){
     console.log(classAge[i]);
     i++;
 }
-console.log("")
-//saco por consola todas las edades pares con while
-i =0;//tengo q reiniciar iantes de cada while
+console.log("Edades pares con WHILE:")//saco por consola todas las edades pares con while
+var i =0;//tengo q reiniciar i antes de cada while
 while(i<classAge.length){
     if((classAge[i] % 2) == 0){
     console.log(classAge[i]);
     }
     i++;
 }
-console.log("")
-//saca por consola todas las edades pares con for
+console.log("Edades pares con FOR:")//saca por consola todas las edades pares con for
 for(var i = 0; i < classAge.length; i++){
     if((classAge[i] % 2) == 0){
         console.log(classAge[i]);
@@ -38,59 +36,54 @@ console.log("")
 
 // ejercicio 3
 var matriz = [29,5,36,9,8,7,25,6,10,4,96,100];
-var numMenor = 0;
-for(var i=0; i < matriz.length; i++){
-    // console.log(matriz);
-    if (matriz[i] < matriz[i+1]) {
-        numMenor = matriz[i] 
-        matriz[i] = matriz[i+1]
-        matriz[i+1] = numMenor
+function numeroMenor(array) {
+    var numMenor=array[0];
+    for (var i= 0; i< array.length; i++) {
+        if (array[i] < numMenor) {
+            numMenor = array[i];
+        } 
     }
-    else{
-        numMenor = matriz[i]
-    }
+    console.log("Numero menor:",numMenor);
 }
-console.log("Numero menor:",numMenor);
+numeroMenor(matriz)
 
 // ejercicio 4
-var numMayor = 0;
-for(var i=0; i < matriz.length; i++){
-    // console.log(matriz);
-    if (matriz[i] > matriz[i+1]) {
-        numMayor = matriz[i] 
-        matriz[i] = matriz[i+1]
-        matriz[i+1] = numMayor
+var matriz = [29,5,36,9,8,7,25,6,10,4,96,100];
+function numeroMayor(array) {
+    var numMayor = array[0];
+    for(var i=0; i < array.length; i++){
+        if (array[i] > numMayor) {
+            numMayor = array[i];
+        } 
     }
-    else{
-        numMayor = matriz[i]
-    }
+    console.log("Numero mayor:",numMayor);
 }
-console.log("Numero mayor:",numMayor);
+numeroMayor(matriz)
 
 // ejercicio 5
-
-// var array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];
-// var index = 1;
-// function posicion (array,index)
-//no entendi muy bien el problema
+var array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];
+var index = 1;
+function posicion(numeros,i) {
+    console.log(numeros[i]);
+}
+posicion(array,index);
 
 // ejercicio 6
+var array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100]; 
+function numeroIgual(array) {
+    var numIgual = [];
+    for (var i = 0; i < array.length; i++) {
+        for (var j = (i+1); j < array.length; j++) {
+            if ( array[i] == array[j]) {
+                numIgual[numIgual.length] = array[i];
+            }
+        }
+    }
+    console.log("Numeros repetidos del array:",numIgual);
+}
+numeroIgual(array);
+//esto solo funciona solo si hay dos numeros iguales, si hay mas el numero se repite
+
+// ejercicio 7
 
 
-
-
-//repaso java clase viernes 2020-07-17
-
-// for(var i = 0; i < 10; i++){
-//     console.log(i)
-// }
-//console.log("")
-//var num = [34,7,4,26,51];
-//console.log(num[2]);
-//console.log(num.length);//lento? cuenta el numero de posiciones q hay 
-//console.log(num[num.length-1]);//con el corchete muestra el contenido de la caja
-//console.log(num[400]);//llama la posicion 400 pero no existe
-//num[6] = 99;//da un valor a la posicion 6
-//console.log(num);//llamar por pantalla
-//num[221] = 90;//da un valor a la posicion 221
-//console.log(num);
