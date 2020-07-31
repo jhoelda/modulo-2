765b7ffc9de1be909391c9160117da2b1aeded74
jhoelda <jhoeldallanos@gmail.com>
2020-07-31 14:14:26 -0300

    terminado tarea 1

--- 2-tarea 1/playground/main - copia.js
+++ 2-tarea 1/playground/main - copia.js
@@ -32,34 +32,32 @@ if (ignasiAge == age) {
 // Funciones de matriz de JavaScript
 // ejercicio 1
 //integrantes de Ctrol Z
-var className = ["Belen","Cata","Jhoelda","Luz","Maga","Yennir","Carla","Rodrigo","Branko"]
-className.sort()
-console.log(className[0]);//primera posicion 
-console.log(className[className.length-1])//ultima posicion
-console.log("")
+var className = ["Belen","Catalina","Jhoelda","Luz","Magali","Yennyfer","Carla","Rodrigo","Branko","Andrea","Aylen","Azucena","Belen","Camila","Carolina","Florencia","Katy","Luana","Macarena","Malena","Maria","Micaela","Nadine","Nicole","Rocio","Reyna","Veronica","Victoria","Yael","Florencia","Janneth","Melissa","Florencia"]
+className.sort()//ordena className alfabeticamente
+console.log("Primer valor del array:",className[0]);//primera posicion 
+console.log("Ultimo valor del array:",className[className.length-1])//ultima posicion
 for(var i = 0; i < className.length; i++){
     console.log(className[i])
 }
+console.log("")//dejo un espacio para separar cada ejercicio en la consola
 
 // ejercicio 2
 var classAge = [20,21,22,23,20,19,24,18,22,20,23,21,23,26,32]
-i =0;
-//saca por pantalla todos las edades
+console.log("Todas las edades:")//saco por consola todos las edades
+var i =0;
 while(i<classAge.length){
     console.log(classAge[i]);
     i++;
 }
-console.log("")
-//saca por pantalla todas las edades pares con while
-i =0;
+console.log("Edades pares con WHILE:")//saco por consola todas las edades pares con while
+var i =0;//tengo q reiniciar i antes de cada while
 while(i<classAge.length){
     if((classAge[i] % 2) == 0){
     console.log(classAge[i]);
     }
     i++;
 }
-console.log("")
-//saca por pantalla todas las edades pares con for
+console.log("Edades pares con FOR:")//saca por consola todas las edades pares con for
 for(var i = 0; i < classAge.length; i++){
     if((classAge[i] % 2) == 0){
         console.log(classAge[i]);
@@ -69,46 +67,132 @@ console.log("")
 
 // ejercicio 3
 var matriz = [29,5,36,9,8,7,25,6,10,4,96,100];
-var numMenor = 0;
-for(var i=0; i < matriz.length; i++){
-    // console.log(matriz);
-    if (matriz[i] < matriz[i+1]) {
-        numMenor = matriz[i] 
-        matriz[i] = matriz[i+1]
-        matriz[i+1] = numMenor
+function numeroMenor(array) {
+    var numMenor=array[0];
+    for (var i= 0; i< array.length; i++) {
+        if (array[i] < numMenor) {
+            numMenor = array[i];
+        } 
     }
-    else{
-        numMenor = matriz[i]
-    }
+    console.log("Numero menor:",numMenor);
 }
-console.log("Numero menor:",numMenor);
+numeroMenor(matriz)
 
 // ejercicio 4
-var numMayor = 0;
-for(var i=0; i < matriz.length; i++){
-    // console.log(matriz);
-    if (matriz[i] > matriz[i+1]) {
-        numMayor = matriz[i] 
-        matriz[i] = matriz[i+1]
-        matriz[i+1] = numMayor
+var matriz = [29,5,36,9,8,7,25,6,10,4,96,100];
+function numeroMayor(array) {
+    var numMayor = array[0];
+    for(var i=0; i < array.length; i++){
+        if (array[i] > numMayor) {
+            numMayor = array[i];
+        } 
     }
-    else{
-        numMayor = matriz[i]
-    }
+    console.log("Numero mayor:",numMayor);
 }
-console.log("Numero mayor:",numMayor);
+numeroMayor(matriz)
 
 // ejercicio 5
-
-// var array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];
-// var index = 1;
-// function posicion (array,index)
-//no entendi muy bien el problema
+var array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];
+var index = 1;
+function posicion(numeros,i) {
+    console.log(numeros[i-1]);
+}
+posicion(array,index);
 
 // ejercicio 6
+var array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100]; 
+function numeroIgual(array) {
+    var numIgual = [];
+    var objetoAux = {};
+    for(var i = 0; i < array.length; i++) {
+        if(!objetoAux[array[i]]) {
+            objetoAux[array[i]] = 0;
+        }
+        objetoAux[array[i]] += 1;
+    }
 
+    for(propiedad in objetoAux) {
+        if(objetoAux[propiedad] > 1) {
+            numIgual.push(propiedad)
+        }
+    }
+    //la palabra propiedad puede ser cambiada, pero se debe usar la misma en las 3
 
+    console.log("Numeros repetidos del array:",numIgual);
+}
+numeroIgual(array)
 
+// ejercicio 7
+
+myColor = ["Red", "Green", "White", "Black"];
+function arrayIntoString(array) {
+    return array.toString()
+}
+console.log(arrayIntoString(myColor))
+
+function arrayToString(array) {
+    var frase = "";
+    for (let i = 0; i < array.length; i++) {
+        frase += '"';
+        frase += array[i];
+        frase += '"';
+        if (i < array.length -1) {//menos 1 porque la ultima no tiene coma
+            frase += ', ';
+        }  
+    }
+    return frase;
+}
+console.log(arrayToString(myColor))
+
+// Funciones de cadena de JavaScript
+// ejercicio 1
+function reverseString(input) {
+    var string = ""+input;
+    var reverse = [];
+    var array = string.split("")//divide el elemento por los espacios
+    for (var i = array.length; i >= 0; i--) {
+        reverse.push(array[i])
+    }
+    return reverse.join("");//une el elemento
+}
+console.log(reverseString("escribir"))
+console.log(parseInt(reverseString(32443),10))
+// typeof()
+// parseInt()
+//buscar: eso pregunta si es un numero para convertilo de nuevo en un numero
+
+// ejercicio 2
+function sortAlphabetically(input) {
+    var string = ""+input;
+    var array = string.split("");
+    return array.sort().join('');//sort: ordena alfabeticamente
+}
+console.log(sortAlphabetically("webmaster"))
+
+// ejercicio 3
+function capitalCase(string) {
+    var enMayuscula = string.split(" ").map((palabra)=>{
+        return palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    }).join(" ");
+    //split: divide el array en los espacios, map:cada elemento en mayuscula
+    return enMayuscula;
+}
+console.log(capitalCase("prince of persia"))
+
+// ejercicio 4
+function palabraLarga(string) {
+    var array = string.split(" ");
+    var contador = 0;
+    var palabraLarga = "";
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > contador) {
+            contador = array[i].length;
+            palabraLarga = array[i]            
+        }        
+    } 
+    return palabraLarga;   
+}
+console.log(palabraLarga("Web Development Tutorial"))
 
 //repaso java clase viernes 2020-07-17
 
--- 2-tarea 1/playground/main.js
+++ 2-tarea 1/playground/main.js
@@ -1,89 +1,49 @@
-// Funciones de matriz de JavaScript
+// Funciones de cadena de JavaScript
 // ejercicio 1
-//integrantes de Ctrol Z
-var className = ["Belen","Catalina","Jhoelda","Luz","Magali","Yennyfer","Carla","Rodrigo","Branko","Andrea","Aylen","Azucena","Belen","Camila","Carolina","Florencia","Katy","Luana","Macarena","Malena","Maria","Micaela","Nadine","Nicole","Rocio","Reyna","Veronica","Victoria","Yael","Florencia","Janneth","Melissa","Florencia"]
-className.sort()//ordena className alfabeticamente
-console.log("Primer valor del array:",className[0]);//primera posicion 
-console.log("Ultimo valor del array:",className[className.length-1])//ultima posicion
-for(var i = 0; i < className.length; i++){
-    console.log(className[i])
+function reverseString(input) {
+    var string = ""+input;
+    var reverse = [];
+    var array = string.split("")//divide el elemento por los espacios
+    for (var i = array.length; i >= 0; i--) {
+        reverse.push(array[i])
+    }
+    return reverse.join("");//une el elemento
 }
-console.log("")//dejo un espacio para separar cada ejercicio en la consola
+console.log(reverseString("escribir"))
+console.log(parseInt(reverseString(32443),10))
+// typeof()
+// parseInt()
+//buscar: eso pregunta si es un numero para convertilo de nuevo en un numero
 
 // ejercicio 2
-var classAge = [20,21,22,23,20,19,24,18,22,20,23,21,23,26,32]
-console.log("Todas las edades:")//saco por consola todos las edades
-var i =0;
-while(i<classAge.length){
-    console.log(classAge[i]);
-    i++;
+function sortAlphabetically(input) {
+    var string = ""+input;
+    var array = string.split("");
+    return array.sort().join('');//sort: ordena alfabeticamente
 }
-console.log("Edades pares con WHILE:")//saco por consola todas las edades pares con while
-var i =0;//tengo q reiniciar i antes de cada while
-while(i<classAge.length){
-    if((classAge[i] % 2) == 0){
-    console.log(classAge[i]);
-    }
-    i++;
-}
-console.log("Edades pares con FOR:")//saca por consola todas las edades pares con for
-for(var i = 0; i < classAge.length; i++){
-    if((classAge[i] % 2) == 0){
-        console.log(classAge[i]);
-        }
-}
-console.log("")
+console.log(sortAlphabetically("webmaster"))
 
 // ejercicio 3
-var matriz = [29,5,36,9,8,7,25,6,10,4,96,100];
-function numeroMenor(array) {
-    var numMenor=array[0];
-    for (var i= 0; i< array.length; i++) {
-        if (array[i] < numMenor) {
-            numMenor = array[i];
-        } 
-    }
-    console.log("Numero menor:",numMenor);
+function capitalCase(string) {
+    var enMayuscula = string.split(" ").map((palabra)=>{
+        return palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    }).join(" ");
+    //split: divide el array en los espacios, map:cada elemento en mayuscula
+    return enMayuscula;
 }
-numeroMenor(matriz)
+console.log(capitalCase("prince of persia"))
 
 // ejercicio 4
-var matriz = [29,5,36,9,8,7,25,6,10,4,96,100];
-function numeroMayor(array) {
-    var numMayor = array[0];
-    for(var i=0; i < array.length; i++){
-        if (array[i] > numMayor) {
-            numMayor = array[i];
-        } 
-    }
-    console.log("Numero mayor:",numMayor);
+function palabraLarga(string) {
+    var array = string.split(" ");
+    var contador = 0;
+    var palabraLarga = "";
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > contador) {
+            contador = array[i].length;
+            palabraLarga = array[i]            
+        }        
+    } 
+    return palabraLarga;   
 }
-numeroMayor(matriz)
-
-// ejercicio 5
-var array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100];
-var index = 1;
-function posicion(numeros,i) {
-    console.log(numeros[i]);
-}
-posicion(array,index);
-
-// ejercicio 6
-var array = [3,6,67,6,23,11,100,8,93,0,17,24,7,1,33,45,28,33,23,12,99,100]; 
-function numeroIgual(array) {
-    var numIgual = [];
-    for (var i = 0; i < array.length; i++) {
-        for (var j = (i+1); j < array.length; j++) {
-            if ( array[i] == array[j]) {
-                numIgual[numIgual.length] = array[i];
-            }
-        }
-    }
-    console.log("Numeros repetidos del array:",numIgual);
-}
-numeroIgual(array);
-//esto solo funciona solo si hay dos numeros iguales, si hay mas el numero se repite
-
-// ejercicio 7
-
-
+console.log(palabraLarga("Web Development Tutorial"))
